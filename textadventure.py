8d356eae13cfc3a105f4aece6116ec197a98af1e
Yleisnero <fischi12325mail.com>
2019-01-25 13:31:31 +0100

    ASCII Chart

--- textadventure.py
+++ textadventure.py
@@ -42,7 +42,25 @@ worldItems = {
         GROUNDDESC: 'A piece of paper lays on the ground',
         SHORTDESC: 'piece of paper',
         LONGDESC: 'The piece of paper says: "Solve the first task to prove you are strong enough to enter this'
-                  ' mysterious world!"',
+                  ' mysterious world!"'
+                  '| 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 |'
+                  '-------------------------------------'
+                  ' 0 |NUL|DLE|SPC| 0 | @ | P | ` | p |'
+                  ' 1 |SOH|DC1| ! | 1 | A | Q | a | q |'
+                  ' 2 |STX|DC2| " | 2 | B | R | b | r |'
+                  ' 3 |ETX|DC3| # | 3 | C | S | c | s |'
+                  ' 4 |EQT|DC4| $ | 4 | D | T | d | t |'
+                  ' 5 |ENQ|NAK| % | 5 | E | U | e | u |'
+                  ' 6 |ACK|SQN| & | 6 | F | V | f | v |'
+                  ' 7 |BEL|BTB| \' | 7 | G | W | g | w |'
+                  ' 8 |BS |CAN| ( | 8 | H | X | h | x |'
+                  ' 9 |HT |EM | ) | 9 | I | Y | i | y |'
+                  ' A |LF |SUB| * | : | J | Z | j | z |'
+                  ' B |VT |ESC| + | ; | K | [ | k | { |'
+                  ' C |FF |FS | , | < | L | \ | l | | |'
+                  ' D |CR |GS | - | = | M | ] | m | } |'
+                  ' E |SO |RS | . | > | N | ^ | n | ~ |'
+                  ' F |SI |US | / | ? | O | _ | o |del|',
         TAKEABLE: True,
         DESCWORDS: ['piece', 'paper', 'pieceofpaper']},
 
@@ -504,7 +522,7 @@ class TextAdventureCmd(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    print('yehdy_vs_thyth_v1 booted ...')
+    print('The ham started burning ...')
     print('====================\n')
     print('(Type "help" for commands.)\n')
     displayLocation(location)
